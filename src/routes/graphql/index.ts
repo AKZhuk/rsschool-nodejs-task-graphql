1dc6c5b7a68440f59d137e4a6de7d75f3803554a
Aliaksei <aliaksei_zhuk@epam.com>
2024-02-28 14:25:03 +0100

    feat: implement queries

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,66 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
+import { GraphQLArgs, graphql } from 'graphql';
+import { UUIDType } from './types/uuid.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+
+  const root: GraphQLArgs["rootValue"] = {
+    posts: () => fastify.prisma.post.findMany(),
+    users: () => fastify.prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true
+      },
+    }),
+    memberTypes: () => fastify.prisma.memberType.findMany(),
+    profiles: () => fastify.prisma.profile.findMany({
+      include: {
+        memberType: true
+      },
+    }),
+    user: async ({ id }: { id: string }) => {
+
+      const user = await fastify.prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+        include: {
+          posts: true,
+          profile: true,
+          userSubscribedTo: true,
+          subscribedToUser: true
+        },
+      });
+      return user
+    },
+    post: async ({ id }: { id: string }) => {
+      const post = await fastify.prisma.post.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      return post
+    },
+    profile: async ({ id }: { id: string }) => {
+      const profile = await fastify.prisma.profile.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      return profile
+    },
+    memberType: async ({ id }: { id: string }) => {
+      const memberType = await fastify.prisma.memberType.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      return memberType
+    },
+    UUIDType,
+  };
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,9 +71,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+      const result = await graphql({
+        schema: schema,
+        source: query,
+        variableValues: variables,
+        rootValue: root,
+      });
+
+      return result
     },
   });
 };
 
 export default plugin;
+
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,5 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { buildSchema } from 'graphql'
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +19,44 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const schemaString = `
+  scalar UUID
+  scalar MemberTypeId
+  type MemberType {
+    id: MemberTypeId!
+    discount: Float
+    postsLimitPerMonth: Float
+  }
+  type Post {
+    id: UUID!
+    title: String!
+    content: String!
+  }
+  type Profile {
+    id: UUID!
+    isMale: Boolean!
+    yearOfBirth: Int!
+    memberType: MemberType
+  }
+   type User {
+    id: UUID!
+    name: String!
+    balance: Float!
+    profile: Profile
+    posts: [Post]
+  }
+  type Query {
+    memberTypes: [MemberType]
+    posts: [Post]
+    users: [User]
+    profiles: [Profile]
+    user(id: UUID!): User
+    memberType(id: MemberTypeId!): MemberType
+    post(id: UUID!): Post
+    profile(id: UUID!): Profile
+  }
+`;
+
+
+export const schema = buildSchema(schemaString);
